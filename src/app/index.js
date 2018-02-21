f15d316380a8599e7cb57d7a5e9ab8aa1af6f32c
ogonzal <ogonzal87@gmail.com>
2018-02-20 18:36:52 -0800

    merged the react-app-starter repo

---
+++ src/app/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+import "../scss/_styles.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hellowwwwwww!</h1>
+      </div>
+    );
+  }
+}
+
+render(<App />, window.document.getElementById("app"));
